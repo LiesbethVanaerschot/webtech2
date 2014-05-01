d57c60907392a79b382e01749b8275f65de81be0
LiesbethVanaerschot <lies.vanaerschot@gmail.com>
2014-05-01 20:58:21 +0200

    committen omdat het percies moet

--- labX/test/public/javascripts/script.js
+++ labX/test/public/javascripts/script.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
         
     });*/
 
+
+	var accordion_head = $('.accordion > li > a');
+	var accordion_body = $('.accordion li > .miniaccordion');
+
+	accordion_head.on('click', function(e){
+		console.log("click2");
+		e.preventDefault();
+		if($(this).attr('class') != 'active'){
+			accordion_body.slideUp('normal');
+			$(this).next().stop(true,true).slideToggle('normal');
+			accordion_head.removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+
 	var accordion_mini = $('.miniaccordion > li > a');
 	var accordion_mini_body = $('.miniaccordion li > .mini-sub');
 
@@ -25,18 +40,24 @@ $(document).ready(function(){
 		}
 	});
 
-	var accordion_head = $('.accordion > li > a');
-	var accordion_body = $('.accordion li > .miniaccordion');
+	/*var accordion_large = $('.large-accordion > li > a');
+	var accordion_inner = $('.large-accordion li > .accordion');
 
-	accordion_head.on('click', function(e){
+	accordion_large.on('click', function(e){
 		e.preventDefault();
+		console.log("click");
 		if($(this).attr('class') != 'active'){
-			accordion_body.slideUp('normal');
+			accordion_inner.slideUp('normal');
 			$(this).next().stop(true,true).slideToggle('normal');
-			accordion_head.removeClass('active');
+			accordion_large.removeClass('active');
 			$(this).addClass('active');
 		}
 	});
 
+	$('#drink').on('click', function(){
+		console.log("click");
+	});*/
+
 });
 //proberen met slide up and down
+//large-accordion toggleclass active
